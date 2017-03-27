6443e49275bd10697282c3e8648fb64448bc82cf
jamescarruthers <jamescarruthers@jamess-mbp.lan>
2017-03-27 23:20:11 +0100

    Changed to .h for #defines

--- software/apSetup.h
+++ software/apSetup.h
@@ -1,3 +1,27 @@
+#define LED1R 9
+#define LED1G 8
+#define LED2R 7
+#define LED2G 6
+#define LED3R 5
+#define LED3G 4
+#define LED4R 3
+#define LED4G 2
+
+#define POT1 A17
+#define POT2 A18
+#define POT3 A19
+#define POT4 A20
+
+#define BUT1 24
+#define BUT2 24
+
+#define UNI1 A2
+#define UNI2 A3
+#define BI1 A0
+#define BI2 A1
+
+#define DAC1 A21
+#define DAC2 A22
 
 void setupAudioPlayground() {
 
---
+++ software/main.ino
@@ -0,0 +1,12 @@
+#include "apSetup.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  setupAudioPlayground();
+
+}
+
+void loop() {
+  // put yourmain code here, to run repeatedly:
+
+}
